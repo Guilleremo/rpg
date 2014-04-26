47f5932ee403371f6c2351fa2ce60332ce9b0c1c
starwarsVrocks <julianinla@me.com>
2014-04-26 12:03:12 -0700

    added stuff 4

--- RPG/public_html/JS.js
+++ RPG/public_html/JS.js
@@ -1,4 +1,9 @@
-function decide(e){
+var txtKeywords = document.getElementById("answer");
+
+txtKeywords.onkeypress = function(e){
+    if(e.keyCode === 13){
         var name=document.getElementById("answer").value;
-        document.getElementById("storyline").innerHTML=name;
+        document.getElementById("storyline").innerHTML="Ahh, "+name+", your one of the fools that came in here to kill the minotaur. I see he has wiped out the rest of your crew, but kept you alive ... how odd, Don't your think?";
+        console.log(name);
     }
+}
