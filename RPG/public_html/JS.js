28cc4467f83b609acbbda3890e9eca49acda2420
starwarsVrocks <julianinla@me.com>
2014-04-26 14:31:50 -0700

    added stuff 6

--- RPG/public_html/JS.js
+++ RPG/public_html/JS.js
@@ -20,13 +20,19 @@ txtKeywords.onkeypress = function(e){
             p.innerHTML="Use the rope on the wall. Well you climb very fast, where to next? (left, right)";
         }
         else if(document.getElementById("answer").value==="another way out"){
-            p.innerHTML="";
+            p.innerHTML="The door has a lock. You can either look for a key or opt to break the door with brute force. (look for key, use brute force";
         }
         if(document.getElementById("answer").value==="left"){
             p.innerHTML="Use the rope on the wall. Well you climb very fast, where to next? (left, right)";
         }
         else if(document.getElementById("answer").value==="right"){
-            p.innerHTML="As you start truning the corner, you see a terrifying monster. Well, you can fight the monster or try and avoid injury and go down the trap door.";
+            p.innerHTML="As you start truning the corner, you see a terrifying monster. Well, you can fight the monster or try and avoid injury and go down the trap door. (fight monster, trap door)";
+        }
+        if(name==="look for key"){
+            p.innerHTML="Do you look to the floor or the wall? (look to floor, look at wall)";
+        }
+        else if(name==="use brute force"){
+            p.innerHTML="Good job! You nearly fell unconscious and lost some health. Do you look for a key or call for help? (look for key, call for help)";
         }
      }
         
