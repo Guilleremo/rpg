0f178d9eb647590009309878a7c2898a2158efc1
starwarsVrocks <julianinla@me.com>
2014-04-26 11:11:07 -0700

    added stuff 3

--- RPG/public_html/JS.js
+++ RPG/public_html/JS.js
@@ -1,6 +1,4 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+function decide(){
+    var name=parseInt(document.getElementById("answer").value);
+    document.getElementById("storyline").innerHTML=name;
+}
